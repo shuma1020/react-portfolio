6735c7c777561ed6f2e7ae57a11be712b9f7add4
shuma1020 <shumaseki40@gmail.com>
2020-01-23 11:42:23 +0900

    fini contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell,List, ListItem, ListItemContent} from 'react-mdl';
 class Contact extends Component {
   render() {
     return (
-      <div className="contactbody">
+      <div className="contact-body">
         <Grid className="contact-grid">
-          <Cell col={6}>half page</Cell>
-          <Cell col={6}>half page</Cell>
+          <Cell col={6}>
+            <h2>Shuma Seki</h2>
+            <img
+              src="https://pbs.twimg.com/profile_images/1219538759663800320/TUOFWSdj_400x400.jpg"
+              alt="avator"
+              style={{height: "250px"}}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>こんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちはこんにちは</p>
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: "30px", fontFamily: "Anton" }}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+
+                    080-5652-1020
+                    </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: "30px", fontFamily: "Anton" }}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    shumaseki40@gmail.com
+                    </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+
+          </Cell>
         </Grid>
       </div>
     )
--- src/components/projects.js
+++ src/components/projects.js
@@ -7,16 +7,49 @@ class Project extends Component {
 
   }
 
-  toggleCategotries() {
+  toggleCategories() {
     if(this.state.activeTab == 0 ) {
+      return (
+        <div className="projects-grid">
+          {/* project #1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* project #2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+
+      )
+    } else if (this.state.activeTab == 1) {
       return (
         <div className="projects-grid">
           {/* project #1 */}
           <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
             <CardTitle style={{
-              color: "afff", height: '176px', background:
-                'url(https://www.kaitoy.xyz/images/react.png) center/ cover'
-            }}>React Project #1</CardTitle>
+              color: "#000", height: '176px', background:
+                'url(https://www.cotegg.com/blog/wp-content/uploads/2018/12/rails_logo.png) center/ cover'
+            }}>Rails Project #1</CardTitle>
             <CardText>
               shumashuma
             </CardText>
@@ -31,9 +64,26 @@ class Project extends Component {
           {/* project #2 */}
           <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
             <CardTitle style={{
+              color: "#000", height: '176px', background:
+                'url(https://www.cotegg.com/blog/wp-content/uploads/2018/12/rails_logo.png) center/ cover'
+            }}>Rails Project #2</CardTitle>
+            <CardText>
+              shumashuma
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+            </CardActions>
+            <CardMenu style={{ color: "afff" }}>
+              <IconButton name="share"/>
+            </CardMenu>
+          </Card >
+
+          {/* project #3 */}
+          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+            <CardTitle style={{
               color: "afff", height: '176px', background:
-                'url(https://www.kaitoy.xyz/images/react.png) center/ cover'
-            }}>React Project #2</CardTitle>
+                'url(https://www.cotegg.com/blog/wp-content/uploads/2018/12/rails_logo.png) center/ cover'
+            }}>Rails Project #3</CardTitle>
             <CardText>
               shumashuma
             </CardText>
@@ -46,30 +96,24 @@ class Project extends Component {
           </Card >
         </div>
 
-      )
-    } else if (this.state.activeTab == 1) {
-      return(
-      <div><h1>This is Ruby on Rails</h1></div>
+
     )}
   }
 
 
   render() {
     return (
-      <div className="category-tabs">
+      <div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
           <Tab>React</Tab>
           <Tab>Ruby on Rails</Tab>
         </Tabs>
 
-        <section className="projects-grid">
           <Grid >
             <Cell col={12}>
-              <div className="content">{this.toggleCategotries()}</div>
+              <div className="content">{this.toggleCategories()}</div>
             </Cell>
           </Grid>
-
-        </section>
       </div>
     )
   }
